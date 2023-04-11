ed48b249822e4578239234f54ef694626b30d052
Anytka <118745184+Anytka07@users.noreply.github.com>
2023-04-11 12:38:16 +0300

    Add files via upload

---
+++ OOP/lb_2/Data/DoctorConfig.cs
@@ -0,0 +1,28 @@
+﻿using hospital.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using hospital.Data.Models;
+using System;
+
+namespace hospital.Data.EntityConfiguration
+{
+    public class DoctorConfig : IEntityTypeConfiguration<Doctor>
+    {
+        public void Configure(EntityTypeBuilder<Doctor> builder)
+        {
+            builder.HasKey(d => d.DoctorId);
+            builder.Property(d => d.Name)
+                .IsRequired(true)
+                .HasColumnName("Name")
+                .HasColumnType("varchar")
+                .HasMaxLength(100)
+                .IsUnicode(true);
+            builder.Property(d => d.Specialty)
+                .IsRequired(true)
+                .HasColumnName("Specialty")
+                .HasColumnType("varchar")
+                .HasMaxLength(100)
+                .IsUnicode(true);
+        }
+    }
+}
