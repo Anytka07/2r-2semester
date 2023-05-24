1d3db92cb5c094a95861d1130ffcb69e7bfb2752
Anytka <118745184+Anytka07@users.noreply.github.com>
2023-05-24 23:17:56 +0300

    Add files via upload

---
+++ OOP/lb_4/P01_BillsPaymentSystem.Data/Configuration.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_BillsPaymentSystem.Data
+{
+    internal class Configuration
+    {
+        public const string ConnectionString = @"Data Source=DESKTOP-1TDJKD4;Database=BillsPaymentMethod;Integrated Security=true;Encrypt=false;TrustServerCertificate=true;";
+    }
+}
